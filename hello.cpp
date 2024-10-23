b175d3423e184388ecd0f0c3115722d89452cc07
Leon050702 <leonchingleonghchen@gmail.com>
2024-10-23 15:28:31 +0800

    testing2

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std ;
+
+
+struct car{
+
+string name ;
+int num ;
+int door ;
+
+};
+
+int main (){
+
+
+car truck ;
+
+cin>>truck.name >>truck.num >>truck.door;
+cout <<truck.name <<truck.num <<truck.door;
+system ("pause");
+
+    return 0;
+}
